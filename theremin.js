2ac36ffd1c57324538573d1b735b91d88f63483d
capravictoriae <pmail314@gmail.com>
2016-06-11 14:25:44 +0200

    Improvement and touch support

--- theremin.js
+++ theremin.js
@@ -1,11 +1,13 @@
 var context = new AudioContext(),
+  gainNode = context.createGain(),
   mousedown = false,
   oscillator = null;
-var gainNode = context.createGain();
+
+gainNode.connect(context.destination);
 
 var calculateFrequency = function (mouseXPosition) {
-  var minFreq = 20;
-  var maxFreq = 2000;
+  var minFreq = 20,
+      maxFreq = 2000;
 
   var actualFreq = ((mouseXPosition / window.innerWidth) * maxFreq) + minFreq;
 
@@ -13,40 +15,77 @@ var calculateFrequency = function (mouseXPosition) {
 };
 
 var calculateGain = function (mouseYPosition) {
-  var minGain = 0;
-  var maxGain = 1;
+  var minGain = 0,
+      maxGain = 1;
 
-  return 1 - ((mouseYPosition / window.innerHeight) * maxGain) + minGain;
+  var actualGain = 1 - ((mouseYPosition / window.innerHeight) * maxGain) + minGain;
+
+  return actualGain;
 };
 
-document.body.addEventListener('mousedown', function(e){
+var createOscillator = function (e) {
+  var xPos = e.clientX,
+      yPos = e.clientY;
 
-  mousedown = true;
-  // Mouse was pressed
-  oscillator = context.createOscillator();
+    if (e.touches) {
+        xPos = e.touches[0].clientX;
+        yPos = e.touches[0].clientY;
+    }
 
-  oscillator.frequency.setTargetAtTime(calculateFrequency(e.clientX), context.currentTime, 0.05);
-  gainNode.gain.setTargetAtTime(calculateFrequency(e.clientY), context.currentTime, 0.05);
+    mousedown = true;
 
-  // Connect the oscillator to the speaker
-  oscillator.connect(gainNode);
-  gainNode.connect(context.destination);
-  // Start the oscillator to make sound (in this precise moment)
-  oscillator.start(context.currentTime);
+    oscillator = context.createOscillator();
+    oscillator.frequency.setTargetAtTime(calculateFrequency(xPos), context.currentTime, 0.001);
+    gainNode.gain.setTargetAtTime(calculateGain(yPos), context.currentTime, 0.001);
+    oscillator.connect(gainNode);
+    oscillator.start(context.currentTime);
+};
+
+var stopOscillator = function () {
+    mousedown = false;
+
+    if (oscillator) {
+        oscillator.stop(context.currentTime);
+        oscillator.disconnect();
+    }
+};
+
+var changeFrequency = function (e) {
+    var xPos = e.clientX,
+        yPos = e.clientY;
+
+    if (e.touches) {
+        xPos = e.touches[0].clientX;
+        yPos = e.touches[0].clientY;
+    }
+
+    if (mousedown && oscillator) {
+         oscillator.frequency.setTargetAtTime(calculateFrequency(xPos), context.currentTime , 0.001);
+         gainNode.gain.setTargetAtTime(calculateGain(yPos), context.currentTime, 0.001);
+    }
+};
+
+document.body.addEventListener('mousedown', function (e) {
+    createOscillator(e);
 });
 
-document.body.addEventListener('mouseup', function(){
-  mousedown = false;
-  // Mouse was released
-  oscillator.stop(context.currentTime);
-  oscillator.disconnect();  
-  
+document.body.addEventListener('touchstart', function (e) {
+    createOscillator(e);
 });
 
-document.body.addEventListener('mousemove', function(e) {
-  if (mousedown) {
-    oscillator.frequency.setTargetAtTime(calculateFrequency(e.clientX), context.currentTime, 0.01);
-    gainNode.gain.setTargetAtTime(calculateFrequency(e.clientY), context.currentTime, 0.05);
-  }
+document.body.addEventListener('mouseup', function () {
+    stopOscillator();
+});
+
+document.body.addEventListener('touchend', function () {
+    stopOscillator();
+});
+
+document.body.addEventListener('mousemove', function (e) {
+    changeFrequency(e);
+});
+
+document.body.addEventListener('touchmove', function (e) {
+    changeFrequency(e);
 });
 
